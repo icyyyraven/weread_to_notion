49714d1c596a5112a03e538f6f84242779412f12
icyyyraven <114460272+icyyyraven@users.noreply.github.com>
2023-12-22 13:10:39 +0800

    Update weread.py
    个人习惯修订

--- weread.py
+++ weread.py
@@ -70,6 +70,7 @@ def get_bookinfo(bookId):
         print(f"get {bookId} book info failed")
         return ("",0)
 
+#不加载目录
 
 def get_review_list(bookId):
     """获取笔记"""
@@ -81,16 +82,6 @@ def get_review_list(bookId):
     reviews = list(map(lambda x: x.get("review"), reviews))
     reviews = list(map(lambda x: {**x, "markText": x.pop("content")}, reviews))
     return summary, reviews
-
-
-def get_table_of_contents():
-    """获取目录"""
-    return {
-        "type": "table_of_contents",
-        "table_of_contents": {
-            "color": "default"
-        }
-    }
 
 
 def get_heading(level, content):
@@ -131,42 +122,23 @@ def get_quote(content):
 
 
 def get_callout(content, style, colorStyle, reviewId):
-    # 根据不同的划线样式设置不同的emoji 直线type=0 背景颜色是1 波浪线是2
-    emoji = "〰️"
-    if style == 0:
-        emoji = "💡"
-    elif style == 1:
-        emoji = "⭐"
-    # 如果reviewId不是空说明是笔记
-    if reviewId != None:
-        emoji = "✍️"
+    # 简化笔记呈现方式
     color = "default"
-    # 根据划线颜色设置文字的颜色
-    if colorStyle == 1:
-        color = "red"
-    elif colorStyle == 2:
-        color = "purple"
-    elif colorStyle == 3:
+    if reviewId != None:
         color = "blue"
-    elif colorStyle == 4:
-        color = "green"
-    elif colorStyle == 5:
-        color = "yellow"
     return {
-        "type": "callout",
-        "callout": {
+        "type": "quote",
+        "quote": {
             "rich_text": [{
                 "type": "text",
                 "text": {
                     "content": content,
                 }
             }],
-            "icon": {
-                "emoji": emoji
-            },
             "color": color
         }
     }
+
 
 
 def check(bookId):
@@ -198,7 +170,8 @@ def get_chapter_info(bookId):
     return None
 
 
-def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating,categories):
+def insert_to_notion(bookName, bookId, cover, author, isbn, rating, categories):
+    # properties中删除不需要的sort
     """插入到notion"""
     time.sleep(0.3)
     parent = {
@@ -209,12 +182,11 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating,categorie
         "BookName": {"title": [{"type": "text", "text": {"content": bookName}}]},
         "BookId": {"rich_text": [{"type": "text", "text": {"content": bookId}}]},
         "ISBN": {"rich_text": [{"type": "text", "text": {"content": isbn}}]},
-        "URL": {"url": f"https://weread.qq.com/web/reader/{calculate_book_str_id(bookId)}"},
         "Author": {"rich_text": [{"type": "text", "text": {"content": author}}]},
-        "Sort": {"number": sort},
         "Rating": {"number": rating},
         "Cover": {"files": [{"type": "external", "name": "Cover", "external": {"url": cover}}]},
     }
+    #不显示URL，新增categories
     if(categories!=None):
         multi_select = [{"name": x} for x in categories]
         properties["Categories"] = {"multi_select":multi_select}
@@ -226,28 +198,20 @@ def insert_to_notion(bookName, bookId, cover, sort, author,isbn,rating,categorie
         format_time = ""
         hour = readingTime // 3600
         if hour > 0:
-            format_time += f"{hour}时"
+            format_time += f"{hour}h"
         minutes = readingTime % 3600 // 60
         if minutes > 0:
-            format_time += f"{minutes}分"
+            format_time += f"{minutes}min"
         properties["Status"] = {"select": {
-            "name": "读完" if markedStatus == 4 else "在读"}}
+            "name": "Finished" if markedStatus == 4 else "Reading"}}
         properties["ReadingTime"] = {"rich_text": [
             {"type": "text", "text": {"content": format_time}}]}
-        properties["Progress"] ={"number": readingProgress}
         if "finishedDate" in read_info:
-            properties["Date"] = {"date": {"start": datetime.utcfromtimestamp(read_info.get(
+            properties["EndDate"] = {"date": {"start": datetime.utcfromtimestamp(read_info.get(
                 "finishedDate")).strftime("%Y-%m-%d %H:%M:%S"), "time_zone": "Asia/Shanghai"}}
-            
-    if(cover.startswith("http")):
-        icon = {
-            "type": "external",
-            "external": {
-                "url": cover
-            }
-        }
-    else:
-        icon = {"type": "emoji","emoji": "📚"}
+
+    icon = {"type": "emoji","emoji": "📖"}
+    #icon简化，不使用书本封面
     # notion api 限制100个block
     response = client.pages.create(
         parent=parent, icon=icon, properties=properties)
@@ -310,8 +274,6 @@ def get_children(chapter, summary, bookmark_list):
     children = []
     grandchild = {}
     if chapter != None:
-        # 添加目录
-        children.append(get_table_of_contents())
         d = {}
         for data in bookmark_list:
             chapterUid = data.get("chapterUid", 1)
@@ -323,15 +285,11 @@ def get_children(chapter, summary, bookmark_list):
                 # 添加章节
                 children.append(get_heading(
                     chapter.get(key).get("level"), chapter.get(key).get("title")))
-            for i in value:
-                if(data.get("reviewId")==None and "style" in i and "colorStyle" in i):
-                    if(i.get("style") not in styles):
-                        continue
-                    if(i.get("colorStyle") not in colors):
-                        continue
-                markText = i.get("markText")
-                for j in range(0, len(markText)//2000+1):
-                    children.append(get_callout(markText[j*2000:(j+1)*2000],i.get("style"), i.get("colorStyle"), i.get("reviewId")))
+            for i in value: 
+                # 这里暂时没有更新最新版
+                callout = get_callout(
+                    i.get("markText"), data.get("style"), i.get("colorStyle"), i.get("reviewId"))
+                children.append(callout)
                 if i.get("abstract") != None and i.get("abstract") != "":
                     quote = get_quote(i.get("abstract"))
                     grandchild[len(children)-1] = quote
@@ -339,22 +297,13 @@ def get_children(chapter, summary, bookmark_list):
     else:
         # 如果没有章节信息
         for data in bookmark_list:
-            if(data.get("reviewId")==None and "style" in data and "colorStyle" in data):
-                if(data.get("style") not in styles):
-                    continue
-                if(data.get("colorStyle") not in colors):
-                    continue
-            markText = data.get("markText")
-            for i in range(0, len(markText)//2000+1):
-                children.append(get_callout(markText[i*2000:(i+1)*2000],
-                                data.get("style"), data.get("colorStyle"), data.get("reviewId")))
+            children.append(get_callout(data.get("markText"),
+                            data.get("style"), data.get("colorStyle"), data.get("reviewId")))
     if summary != None and len(summary) > 0:
         children.append(get_heading(1, "点评"))
         for i in summary:
-            content = i.get("review").get("content")
-            for j in range(0, len(content)//2000+1):
-                children.append(get_callout(content[j*2000:(j+1)*2000], i.get(
-                    "style"), i.get("colorStyle"), i.get("review").get("reviewId")))
+            children.append(get_callout(i.get("review").get("content"), i.get(
+                "style"), i.get("colorStyle"), i.get("review").get("reviewId")))
     return children, grandchild
 
 def transform_id(book_id):
@@ -397,48 +346,15 @@ def calculate_book_str_id(book_id):
     result += md5.hexdigest()[0:3]
     return result
 
-def download_image(url, save_dir='cover'):
-    # 确保目录存在，如果不存在则创建
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    # 获取文件名，使用 URL 最后一个 '/' 之后的字符串
-    file_name = url.split('/')[-1]+".jpg"
-    save_path = os.path.join(save_dir, file_name)
-
-    # 检查文件是否已经存在，如果存在则不进行下载
-    if os.path.exists(save_path):
-        print(f"File {file_name} already exists. Skipping download.")
-        return save_path
-
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=128):
-                file.write(chunk)
-        print(f"Image downloaded successfully to {save_path}")
-    else:
-        print(f"Failed to download image. Status code: {response.status_code}")
-    return save_path
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("weread_cookie")
     parser.add_argument("notion_token")
     parser.add_argument("database_id")
-    parser.add_argument("ref")
-    parser.add_argument("repository")
-    parser.add_argument("--styles",nargs="+",type=int,help="划线样式")
-    parser.add_argument("--colors",nargs="+",type=int,help="划线颜色")
     options = parser.parse_args()
     weread_cookie = options.weread_cookie
     database_id = options.database_id
     notion_token = options.notion_token
-    ref = options.ref
-    branch = ref.split('/')[-1]
-    repository = options.repository
-    styles = options.styles
-    colors = options.colors
     session = requests.Session()
     session.cookies = parse_cookie_string(weread_cookie)
     client = Client(
@@ -448,22 +364,14 @@ if __name__ == "__main__":
     session.get(WEREAD_URL)
     latest_sort = get_sort()
     books = get_notebooklist()
-    i = 0
     if (books != None):
         for book in books:
-            i +=1
             sort = book["sort"]
             if sort <= latest_sort:
                 continue
             book = book.get("book")
             title = book.get("title")
             cover = book.get("cover")
-            if book.get("author") == "公众号" and book.get("cover").endswith("/0"):
-                cover += ".jpg"
-            if(cover.startswith("http") and not cover.endswith(".jpg")):
-                path = download_image(cover)
-                cover = f"https://raw.githubusercontent.com/{repository}/{branch}/{path}"
-                print(cover)
             bookId = book.get("bookId")
             author = book.get("author")
             categories = book.get("categories")
@@ -472,7 +380,8 @@ if __name__ == "__main__":
             print(f"正在同步 {title} ,一共{len(books)}本，当前是第{i}本。")
             check(bookId)
             isbn,rating = get_bookinfo(bookId)
-            id = insert_to_notion(title, bookId, cover, sort, author,isbn,rating,categories)
+            id = insert_to_notion(title, bookId, cover, sort, author,isbn,rating)
+            results = add_children(id, children)
             chapter = get_chapter_info(bookId)
             bookmark_list = get_bookmark_list(bookId)
             summary, reviews = get_review_list(bookId)
@@ -481,6 +390,6 @@ if __name__ == "__main__":
                 x.get("chapterUid", 1), 0 if (x.get("range", "") == "" or x.get("range").split("-")[0]=="" ) else int(x.get("range").split("-")[0])))
             children, grandchild = get_children(
                 chapter, summary, bookmark_list)
-            results = add_children(id, children)
+            
             if(len(grandchild)>0 and results!=None):
                 add_grandchild(grandchild, results)
